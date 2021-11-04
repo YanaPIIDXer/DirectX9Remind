7ce650b6479947e94aadf91ca50c977cb4e62d02
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-04 21:20:07 +0900

    シーン遷移の仕組みを準備

--- Core/GameFramework/Scene.cpp
+++ Core/GameFramework/Scene.cpp
@@ -3,10 +3,30 @@
 
 // コンストラクタ
 Scene::Scene()
+	: pNextScene(nullptr)
 {
 }
 
 // デストラクタ
 Scene::~Scene()
 {
+	releaseNextScene();
+}
+
+
+// 次のシーンをセット
+void Scene::setNextScene(Scene* pScene)
+{
+	releaseNextScene();		// もし既にセットされていた場合は解放
+	pNextScene = pScene;
+}
+
+// 次のシーンを解放
+void Scene::releaseNextScene()
+{
+	if (pNextScene != nullptr)
+	{
+		delete pNextScene;
+		pNextScene = nullptr;
+	}
 }
--- Core/GameFramework/Scene.h
+++ Core/GameFramework/Scene.h
@@ -12,9 +12,30 @@ public:
 	// デストラクタ
 	virtual ~Scene() = 0;
 
+	// 次のシーンが存在するか？
+	bool HasNextScene() const { return (pNextScene != nullptr); }
+	
+	// 次のシーンを回収
+	Scene* SweepNextScene()
+	{
+		Scene* pScene = pNextScene;
+		pNextScene = nullptr;
+		return pScene;
+	}
+
 protected:
 
+	// 次のシーンをセット
+	void setNextScene(Scene* pScene);
+
 private:
+
+	// 次のシーン
+	Scene* pNextScene;
+
+
+	// 次のシーンを解放
+	void releaseNextScene();
 };
 
 #endif		// #ifndef SCENE_H
